e12dd141a6772ab9568f6f60d7fc71c7bde13218
Velenir <Velenir@users.noreply.github.com>
2017-08-26 00:46:44 +0300

    feat(Setup the whole App together):

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,24 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import './App.scss';
+import ContactsTable from '../ContactsTable';
+import { createContact } from '../../store/actions';
+import Modal from '../Modal';
+import ContactsForm from '../Modal/ContactsForm';
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-}
+// need to wrap in a func to allow passing class components
+const createModalForm = () => <ContactsForm />;
+
+
+const App = ({ createContact }) => (
+  <div className="app">
+    <Modal>
+      {createModalForm}
+    </Modal>
+    <h1 className="title">Gnosis Frontend - React/Redux Task</h1>
+    <button onClick={createContact} className="btn">Create new Contact</button>
+    <ContactsTable />
+  </div>
+);
+
+export default connect(null, { createContact })(App);
