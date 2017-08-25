9f75658f719ce9602a2b78503413d70f2e140fef
Velenir <Velenir@users.noreply.github.com>
2017-08-25 06:11:48 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from 'react-hot-loader/lib/AppContainer';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import createStore from './store';
+
+import './styles/main.scss';
+
+const store = createStore();
+
+const render = App =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <AppContainer>
+        <App />
+      </AppContainer>
+    </Provider>,
+    document.getElementById('app')
+  );
+
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept('./components/App', () => {
+    const App = require('./components/App').default;
+    render(App);
+  });
+}
+
+render(App);
