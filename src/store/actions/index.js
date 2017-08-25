efd378b4757fef33db0acab617de462ca1e66126
Velenir <Velenir@users.noreply.github.com>
2017-08-26 00:41:15 +0300

    feat: Add actions

---
+++ src/store/actions/index.js
@@ -0,0 +1,27 @@
+import uuid from 'uuid/v4';
+import * as TYPES from './types';
+
+export const createContact = () => ({
+  type: TYPES.CREATE_CONTACT,
+  payload: { id: uuid() }
+});
+export const saveContact = (id) => ({
+  type: TYPES.SAVE_CONTACT,
+  payload: { id }
+});
+export const editContact = (id) => ({
+  type: TYPES.EDIT_CONTACT,
+  payload: { id }
+});
+export const editContactProp = (id, name, value) => ({
+  type: TYPES.EDIT_CONTACT_PROP,
+  payload: { id, name, value }
+});
+export const deleteContact = (id) => ({
+  type: TYPES.DELETE_CONTACT,
+  payload: { id }
+});
+export const cancelEditSave = (id) => ({
+  type: TYPES.CANCEL_EDIT_SAVE,
+  payload: { id }
+});
---
+++ src/store/actions/types.js
@@ -0,0 +1,6 @@
+export const CREATE_CONTACT = Symbol.for('CREATE_CONTACT');
+export const SAVE_CONTACT = Symbol.for('SAVE_CONTACT');
+export const EDIT_CONTACT = Symbol.for('EDIT_CONTACT');
+export const EDIT_CONTACT_PROP = Symbol.for('EDIT_CONTACT_PROP');
+export const DELETE_CONTACT = Symbol.for('DELETE_CONTACT');
+export const CANCEL_EDIT_SAVE = Symbol.for('CANCEL_EDIT_SAVE');
